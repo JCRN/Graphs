6a3421ce3da77f2ff09e722b423caad91a6d88fb
jcrn <clemons.jason@gmail.com>
2020-01-22 13:52:55 -0800

    update

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -7,32 +7,35 @@ def earliest_ancestor(ancestors, starting_node):
     # Init graph
     ancestor_tree = Graph()
     
-    # Iterate through ancestors
+    # Add vertices to graph (ancestor_tree)
     for (parent, child) in ancestors:  
-        # Add vertices to ancestor_tree
         ancestor_tree.add_vertex(parent)
         ancestor_tree.add_vertex(child)
     
+    # Add edges
     for (parent, child) in ancestors:
-        # Add edges
         ancestor_tree.add_edge(parent, child)
 
-    # Keep track of # ancestors; highest means most ancestors
-    longest_path = 1      
+    # Keep track of # ancestors
+    longest_path = 1
+          
     # Store last node (as an integer)
     earliest_ancestor = 0 
     
     for vertex in ancestor_tree.vertices:
         path = ancestor_tree.dfs(vertex, starting_node) 
-        print('path', path)
+        
         if path:
             # If list length is greater than longest path  
             if len(path) > longest_path:
+                
                 # Set longest path equal to list length  
                 longest_path = len(path)
-                # Set earliest_ancestor equal to current node/vertex  
+                
+                # Set earliest_ancestor equal to current vertex  
                 earliest_ancestor = vertex 
-        # If path is 'None' and 'longest_path' is our default of 1          
+       
+        # If path is 'None' and 'longest_path' is 1, return -1           
         elif not path and longest_path == 1:    
             earliest_ancestor = -1
                 
