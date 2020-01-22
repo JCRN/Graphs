1a0d125df0963f768d2949bd0c99159e5e54e4ff
jcrn <clemons.jason@gmail.com>
2020-01-22 13:55:41 -0800

    update

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,8 +1,6 @@
 
 from util import Stack, Graph
 
-ancestors_data = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
-
 def earliest_ancestor(ancestors, starting_node):
     # Init graph
     ancestor_tree = Graph()
